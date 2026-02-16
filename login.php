46810340e6d6f80fac5cdb117997c4f4e6d84f25
Leogeox <leovginx@gmail.com>
2026-02-16 12:28:15 +0100

    login form

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+// session_start();
+
+// if (!isset($_SESSION['token_admin']) || empty($_SESSION['token_article_add'])) {
+//     $_SESSION['token_article_add'] = bin2hex(random_bytes(32));
+// }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <input type="text" name="email" id="email" placeholder="Insert your email">
+        <input type="text" name="password" id="password" placeholder="Create your password">
+        <br>
+        <button type="submit"></button>
+    </form>
+</body>
+
+</html>
